cfdd106b8ae47262047ec7b1546fa3aa7a3b9568
AashayBharadwaj <64611037+AashayBharadwaj@users.noreply.github.com>
2021-06-15 17:18:34 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+       <h1> URL Shortner </h1>
+
+        <div className = "form">
+        
+       
+        <input type= "text" name='url' >
+        </input>
+       
+       
+        <button className="button">Submit</button>        
+
+
+        </div>
+
+    
     </div>
   );
 }
